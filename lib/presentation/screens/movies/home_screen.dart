a1e925f00cfe582fe73f674099a8f3dff182f218
Alexhr <alex.hernandez24kb@gmail.com>
2024-07-21 17:59:37 -0600

    Carrusel de peliculas

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -38,17 +38,7 @@ class _HomeViewState extends ConsumerState<_HomeView> {
 
         CustomAppbar(),
 
-        Expanded(
-          child: ListView.builder(
-            itemCount: nowPlayingMovies.length,
-            itemBuilder: (context, index) {
-              final movie = nowPlayingMovies[index];
-              return ListTile(
-                title: Text(movie.title),
-              );
-            },
-          ),
-        )
+        MoviesSlideshow(movies: nowPlayingMovies)
       ],
     );
   }
--- lib/presentation/widgets/widgets.dart
+++ lib/presentation/widgets/widgets.dart
@@ -1,2 +1,2 @@
 export 'shared/custom_appbar.dart';
-
+export 'movies/movies_slidesshow.dart';
