9b3a5b3e7e350439513d3af3115cde8b7e8dce59
Alexhr <alex.hernandez24kb@gmail.com>
2024-07-21 21:30:49 -0600

    Evitar peticiones simultáneas

--- lib/presentation/providers/movies/movies_providers.dart
+++ lib/presentation/providers/movies/movies_providers.dart
@@ -18,6 +18,7 @@ typedef MovieCallback = Future<List<Movie>> Function({ int page});
 class MoviesNotifier extends StateNotifier<List<Movie>> {
 
   int currentPage = 0;
+  bool isLoading = false;
   MovieCallback fetchMoreMovies;
 
   MoviesNotifier({
@@ -26,10 +27,15 @@ class MoviesNotifier extends StateNotifier<List<Movie>> {
 
   Future<void> loadNextPage() async{
 
-    currentPage++;
+    if(isLoading) return;
 
+    isLoading = true;
+    print('Loading more movies');
+    currentPage++;
     final List<Movie> movies = await fetchMoreMovies(page: currentPage);
     state = [...state, ...movies];
+    await Future.delayed(const Duration(milliseconds: 300));
+    isLoading = false;
 
   }
 
