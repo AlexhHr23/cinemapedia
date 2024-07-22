1b3fa9932ec616b7bef4c12338202aa42d469fa1
Alexhr <alex.hernandez24kb@gmail.com>
2024-07-21 20:58:25 -0600

    HumanFormats

--- lib/presentation/widgets/movies/movie_horizontal_listview.dart
+++ lib/presentation/widgets/movies/movie_horizontal_listview.dart
@@ -1,4 +1,5 @@
 import 'package:animate_do/animate_do.dart';
+import 'package:cinemapedia/config/helpers/human_formats.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
 
@@ -93,14 +94,18 @@ class _Slide extends StatelessWidget {
           ),
 
           //*Rating
-          Row(
-            children: [
-              Icon(Icons.star_half_outlined, color: Colors.yellow.shade800),
-              const SizedBox(width: 3),
-              Text('${ movie.voteAverage }', style: textStyle.bodyMedium?.copyWith(color: Colors.yellow.shade800)),
-               const SizedBox(width: 10),
-              Text('${movie.popularity}', style: textStyle.bodySmall)
-            ],
+          SizedBox(
+            width: 150,
+            child: Row(
+              children: [
+                Icon(Icons.star_half_outlined, color: Colors.yellow.shade800),
+                const SizedBox(width: 3),
+                Text('${ movie.voteAverage }', style: textStyle.bodyMedium?.copyWith(color: Colors.yellow.shade800)),
+                const SizedBox(width: 30),
+                 Text(HumanFormats.number(movie.popularity), style: textStyle.bodySmall)
+                //Text('${movie.popularity}', style: textStyle.bodySmall)
+              ],
+            ),
           )
         ],
       ),
