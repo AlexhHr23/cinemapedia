e61c5392e5ab69357bde6ce5ead23e27c95765eb
Alexhr <alex.hernandez24kb@gmail.com>
2024-07-28 09:34:23 -0600

    Probar obtención de actores

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -1,4 +1,5 @@
 import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/presentation/providers/actors/actors_by_movie_provider.dart';
 import 'package:cinemapedia/presentation/providers/movies/movie_info_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -26,6 +27,7 @@ class MovieScreenState extends ConsumerState<MovieScreen> {
     super.initState();
 
     ref.read(movieInfoProvider.notifier).loadMovie(widget.movieId);
+    ref.read(actorsByMovieProvider.notifier).loadActors(widget.movieId);
   }
 
 
@@ -57,7 +59,7 @@ class _MovieDetails extends StatelessWidget {
 
   final Movie movie;
 
-  const _MovieDetails({required this.movie});
+  const _MovieDetails({super.key, required this.movie});
 
   @override
   Widget build(BuildContext context) {
