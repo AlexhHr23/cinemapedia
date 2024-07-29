bf4b7e9990d40801d1619a343e06af63ac5d948c
Alexhr <alex.hernandez24kb@gmail.com>
2024-07-28 19:57:06 -0600

    Leading y actions del seach delegate

--- lib/presentation/delegates/search_movie_delagate.dart
+++ lib/presentation/delegates/search_movie_delagate.dart
@@ -1,21 +1,34 @@
 
+import 'package:animate_do/animate_do.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
 
-class SearchMovieDelegate extends SearchDelegate {
+class SearchMovieDelegate extends SearchDelegate<Movie?> {
 
   @override
   String get searchFiledLabel => 'Buscar pélicula';
 
   @override
   List<Widget>? buildActions(BuildContext context) {
+    print('query');
+
     return [
-      const Text('BuildActions')
+      FadeIn(
+        animate: query.isNotEmpty,
+        child: IconButton(
+          onPressed: () => query = '',
+          icon: const Icon(Icons.clear)
+          ),
+      )
     ];
   }
 
   @override
   Widget? buildLeading(BuildContext context) {
-    return const Text('buildLeading');
+    return IconButton(
+      onPressed: () => close(context, null), 
+      icon: const Icon(Icons.arrow_back)
+      );
   }
 
   @override
