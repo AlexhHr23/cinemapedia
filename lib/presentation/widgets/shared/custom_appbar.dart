7927960cb3b94697eae8ac02a6fdaea411a81ba3
Alexhr <alex.hernandez24kb@gmail.com>
2024-08-21 20:56:45 -0600

    Favorite MoviesProvider

--- lib/presentation/widgets/shared/custom_appbar.dart
+++ lib/presentation/widgets/shared/custom_appbar.dart
@@ -42,7 +42,7 @@ class CustomAppbar extends ConsumerWidget {
                     ).then((movie) {
                       if(movie == null ) return;
                       // ignore: use_build_context_synchronously
-                      context.push('/movie/${movie.id}');
+                      context.push('/home/0/movie/${movie.id}');
                     });
 
                   }, 
